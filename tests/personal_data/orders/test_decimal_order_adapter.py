44d7da7a1385d042b62a8395dd79faf32900cb39
Max <max@tfq.at>
2024-11-11 15:25:40 +0100

    [Orders] handle MissingMinimalExchangeTradeVolume

--- tests/personal_data/orders/test_decimal_order_adapter.py
+++ tests/personal_data/orders/test_decimal_order_adapter.py
@@ -21,6 +21,7 @@ import decimal
 from octobot_trading.enums import ExchangeConstantsMarketStatusColumns as Ecmsc
 import octobot_trading.personal_data as personal_data
 import octobot_trading.constants as constants
+import octobot_trading.errors as errors
 import octobot_trading.enums as enums
 from tests import event_loop
 
@@ -172,8 +173,11 @@ async def test_decimal_check_and_adapt_order_details_if_necessary():
     # invalid cost <
     quantity = decimal.Decimal(str(0.5))
     price = decimal.Decimal(str(1))
-    assert personal_data.decimal_check_and_adapt_order_details_if_necessary(quantity, price, symbol_market) == []
-
+    try:
+        personal_data.decimal_check_and_adapt_order_details_if_necessary(quantity, price, symbol_market)
+        assert False
+    except errors.MissingMinimalExchangeTradeVolume:
+        assert True
     # invalid cost >
     quantity = decimal.Decimal(str(10))
     price = decimal.Decimal(str(49))
